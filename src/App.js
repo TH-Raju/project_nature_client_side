7750c434127b00fd4ac7687e46f7a383fc624331
TH-Raju <rajukhan894200@gmail.com>
2023-03-08 20:07:33 +0600

    gallery section design done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Banner from './components/Banner';
 import BanTitle from './components/BanTitle';
+import Gallery from './components/Gallery';
 
 function App() {
   return (
     <div className='mb-96'>
       <Banner></Banner>
       <BanTitle></BanTitle>
+      <Gallery></Gallery>
     </div>
   );
 }
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,7 +3,21 @@ import React from 'react';
 const Gallery = () => {
     return (
         <div>
-
+            <h2 className="text-4xl font-bold text-center mt-36">Nature Gallery</h2>
+            <section className="py-6">
+                <div className="container grid grid-cols-2 gap-4 p-4 mx-auto md:grid-cols-4">
+                    <img src={require('../img/pahar.jpg')} alt="" className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-3 md:row-start-1 dark:bg-gray-500 aspect-square" />
+                    <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src={require('../img/manhand.jpg')} />
+                    <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src={require('../img/jhorna.jpg')} />
+                    <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src={require('../img/little.jpg')} />
+                    <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src={require('../img/moon.jpg')} />
+                    <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src={require('../img/nature.jpg')} />
+                    <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src={require('../img/pata.jpg')} />
+                    <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src={require('../img/tree.jpg')} />
+                    <img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src={require('../img/jhorna.jpg')} />
+                    <img src={require('../img/three.jpg')} alt="" className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-1 md:row-start-3 dark:bg-gray-500 aspect-square" />
+                </div>
+            </section>
         </div>
     );
 };
