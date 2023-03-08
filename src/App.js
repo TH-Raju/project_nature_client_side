bfece8b76c701ffcd93438b5917bdd2f86d0e55a
TH-Raju <rajukhan894200@gmail.com>
2023-03-08 20:14:37 +0600

    footer section design done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import Banner from './components/Banner';
 import BanTitle from './components/BanTitle';
+import Footer from './components/Footer';
 import Gallery from './components/Gallery';
 
 function App() {
   return (
-    <div className='mb-96'>
+    <div>
       <Banner></Banner>
       <BanTitle></BanTitle>
       <Gallery></Gallery>
+      <Footer></Footer>
     </div>
   );
 }
