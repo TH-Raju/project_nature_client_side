f8b9ab2cb1fa0a26441b814e84da18a0d8898ff0
TH-Raju <rajukhan894200@gmail.com>
2023-03-10 15:03:55 +0600

    image added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,25 @@ import Gallery from './components/Gallery';
 
 function App() {
   return (
-    <div>
+    <div >
+      <div className='bg-white flex justify-around '>
+        <div>
+          <img src={require("../src/img/leaf.png")} className="max-h-10 max-w-10 absolute bg-gray-400" alt="" />
+        </div>
+        <div>
+          <img src={require("../src/img/leaf1.png")} className="max-h-10 max-w-10 absolute bg-gray-400" alt="" />
+        </div>
+        <div>
+          <img src={require("../src/img/leaf2.png")} className="max-h-10 max-w-10 absolute bg-gray-400" alt="" />
+        </div>
+        <div>
+          <img src={require("../src/img/leaf3.png")} className="max-h-10 max-w-10 absolute bg-gray-400" alt="" />
+        </div>
+        <div>
+          <img src={require("../src/img/leaf.png")} className="max-h-10 max-w-10 absolute bg-gray-400" alt="" />
+        </div>
+
+      </div>
       <Banner></Banner>
       <BanTitle></BanTitle>
       <Gallery></Gallery>
